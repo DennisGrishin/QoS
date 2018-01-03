ad751f5fe93ee6b80389bf2b56e0cca0c0b08f9a
DennisGrishin <DennisGrishin@users.noreply.github.com>
2018-01-03 09:29:59 +0300

    now they can shoot

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 // Dependencies
-//console.log("Hello!");
 var express = require('express');
 var http = require('http');
 var path = require('path');
@@ -31,53 +30,134 @@ io.on('connection', function(socket) {
 }, 1000);
 */
 
-var players = {};
-io.on('connection', function(socket) {
-  socket.on('new player', function() {
-    players[socket.id] = {
-      x: 300,
-      y: 300,
-	  r: Math.floor(Math.random()*256),
-	  g: Math.floor(Math.random()*256),
-	  b: Math.floor(Math.random()*256)
-    };
-  });
-  socket.on('movement', function(data) {
-    var player = players[socket.id] || {};
-	var LEFT_BORDER = 0;
-	var RIGHT_BORDER = 800;
-	var TOP_BORDER = 0;
-	var BOTTOM_BORDER = 600;
-	var step = 5;
-    if (data.left) {
-      player.x -= step;
-	  if (player.x < LEFT_BORDER) {
-		player.x = RIGHT_BORDER;
-	  }
+var LEFT_BORDER = 0;
+var RIGHT_BORDER = 800;
+var TOP_BORDER = 0;
+var BOTTOM_BORDER = 600;
 
-    }
-    if (data.up) {
-		if (player.y < TOP_BORDER) {
-			player.y = BOTTOM_BORDER;
+var players = {};
+var bullets = {};
+var game_state = {
+	players: {},
+	bullets: {}
+};
+
+function spawn() {
+	return {
+		x: LEFT_BORDER + (RIGHT_BORDER - LEFT_BORDER)*Math.random(),
+		y: TOP_BORDER + (BOTTOM_BORDER - TOP_BORDER)*Math.random(),
+		size: 30,
+		direction: Math.round(2*Math.PI*Math.random()*64)/64,
+		r: Math.floor(Math.random()*256),
+		g: Math.floor(Math.random()*256),
+		b: Math.floor(Math.random()*256),
+		score: 0
+	};
+}
+
+function respawn(player) {
+	player.x = LEFT_BORDER + (RIGHT_BORDER - LEFT_BORDER)*Math.random();
+	player.y = TOP_BORDER + (BOTTOM_BORDER - TOP_BORDER)*Math.random();
+	player.direction = Math.round(2*Math.PI*Math.random()*64)/64;
+}
+
+io.on('connection', function(socket) {
+	socket.on('new player', function() {
+		game_state.players[socket.id] = spawn();
+	});
+	
+	socket.on('movement', function(data) {
+		var player = game_state.players[socket.id] || {};
+		var step = 5;
+		var rotation_step = 2*Math.PI/64;
+		var size = player.size;
+		if (data.left) {
+			player.direction += rotation_step;
 		}
-		player.y -= step;
-    }
-    if (data.right) {
-		if (player.x > RIGHT_BORDER) {
-			player.x = LEFT_BORDER;
+		if (data.right) {
+			player.direction -= rotation_step;
 		}
-		player.x += step;
-    }
-    if (data.down) {
-		if (player.y > BOTTOM_BORDER) {
-			player.y = TOP_BORDER;
+		
+		if (data.up) {
+			player.x += step*Math.cos(player.direction);
+			player.y -= step*Math.sin(player.direction);
 		}
-		player.y += step;
-    }
-  });
+		
+		if (data.down) {
+			player.x -= step*Math.cos(player.direction);
+			player.y += step*Math.sin(player.direction);
+		}
+		
+		// Border conditions
+		if (player.x < LEFT_BORDER + size/2) {
+			player.x = LEFT_BORDER + size/2;
+		}
+		if (player.y < TOP_BORDER + size/2) {
+			player.y = TOP_BORDER + size/2;
+		}
+		if (player.x > RIGHT_BORDER - size/2) {
+			player.x = RIGHT_BORDER - size/2;
+		}
+		if (player.y > BOTTOM_BORDER - size/2) {
+			player.y = BOTTOM_BORDER - size/2;
+		}
+		
+		// Bullet
+		var bullet_speed = step*1.5;
+		var bullet = game_state.bullets[socket.id];
+		if (bullet) {
+			bullet.x += bullet_speed*Math.cos(bullet.direction);
+			bullet.y -= bullet_speed*Math.sin(bullet.direction);
+			if (bullet.x < LEFT_BORDER || 
+				bullet.x > RIGHT_BORDER ||
+				bullet.y < TOP_BORDER ||
+				bullet.y > BOTTOM_BORDER) {
+				delete game_state.bullets[socket.id];
+			}
+		}
+		else {
+			if (data.space) {
+				var dist = 35;
+				var shift = 10;
+				//var ini_x = player.x + shift;
+				//var ini_y = player.y - dist;
+				game_state.bullets[socket.id] = {
+					x: player.x + shift*Math.sin(player.direction) + dist*Math.cos(player.direction),
+					y: player.y + shift*Math.cos(player.direction) - dist*Math.sin(player.direction),
+					direction: player.direction
+				}
+			}
+		}
+		
+		// Destroy conditions
+		for (var id in game_state.bullets) {
+			var killer = game_state.bullets[id];
+			var relpos = {
+				x: killer.x - player.x,
+				y: killer.y - player.y
+			}
+			if (relpos.x*relpos.x + relpos.y*relpos.y < size*size/4) {
+				//player.x = 300;
+				//player.y = 300;
+				//io.sockets.emit('message', 'hi!');
+				if (game_state.players[id]) {
+					game_state.players[id].score += 1;
+				}
+				respawn(game_state.players[socket.id]);
+			}
+			//io.sockets.emit('message', ' ' + Math.sqrt(relpos.x*relpos.x + relpos.y*relpos.y));
+		}
+	});
+	
+	socket.on('disconnect', function() {
+		delete game_state.players[socket.id];
+	});
 });
+
+
+  
 setInterval(function() {
-  io.sockets.emit('state', players);
+  io.sockets.emit('state', game_state);
 }, 1000 / 60);
 
 
--- static/game.js
+++ static/game.js
@@ -1,17 +1,23 @@
 var socket = io();
+
 socket.on('message', function(data) {
-  console.log(data);
+	console.log(data);
 });
 
 var movement = {
-  up: false,
-  down: false,
-  left: false,
-  right: false
+	up: false,
+	down: false,
+	left: false,
+	right: false,
+	space: false
 }
+
 document.addEventListener('keydown', function(event) {
   switch (event.keyCode) {
-    case 65: // A
+    case 32: // space
+      movement.space = true;
+      break;
+	case 65: // A
       movement.left = true;
       break;
     case 87: // W
@@ -23,10 +29,15 @@ document.addEventListener('keydown', function(event) {
     case 83: // S
       movement.down = true;
       break;
+	
   }
 });
+
 document.addEventListener('keyup', function(event) {
   switch (event.keyCode) {
+	case 32: // space
+      movement.space = false;
+      break;
     case 65: // A
       movement.left = false;
       break;
@@ -43,24 +54,70 @@ document.addEventListener('keyup', function(event) {
 });
 
 socket.emit('new player');
-setInterval(function() {
-  socket.emit('movement', movement);
-}, 1000 / 60);
+
+//setInterval(function() {
+  //socket.emit('movement', movement);
+//}, 1000 / 60);
 
 
 var canvas = document.getElementById('canvas');
 canvas.width = 800;
 canvas.height = 600;
 var context = canvas.getContext('2d');
-socket.on('state', function(players) {
-  context.clearRect(0, 0, 800, 600);
-  for (var id in players) {
-    var player = players[id];
-	context.fillStyle = 'rgb(' + player.r + ', ' + player.g + ', ' + player.b + ')';
-    context.beginPath();
-    context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
-    context.fill();
-  }
+socket.on('state', function(game_state) {
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	for (var id in game_state.players) {
+		var player = game_state.players[id];
+		
+		var x = player.x;
+		var y = player.y;
+		var radius = player.size/2;
+		var shift = 2;
+		var gradient = context.createRadialGradient(x - shift, y - shift, radius, x - shift, y - shift, 0);
+		var color = {
+			r: player.r,
+			g: player.g,
+			b: player.b
+		};
+		var glare = {
+			r: Math.round((player.r + 256)/2),
+			g: Math.round((player.g + 256)/2),
+			b: Math.round((player.b + 256)/2)
+		};
+		gradient.addColorStop(0, 'rgb(' + color.r + ', ' + color.g + ', ' + color.b + ')');
+		gradient.addColorStop(1, 'rgb(' + glare.r + ', ' + glare.g + ', ' + glare.b + ')');
+		context.fillStyle = gradient;
+		context.beginPath();
+		context.arc(x, y, radius, 0, 2*Math.PI);
+		context.fill();
+		
+		var gun = {
+			length: player.size,
+			width: player.size/4,
+			color: 'rgb(80,30,0)'
+		};
+		context.translate(x, y);
+		context.rotate(-player.direction);
+		
+		context.fillStyle = gun.color;
+		context.fillRect(0, gun.width, gun.length, gun.width);
+		context.setTransform(1, 0, 0, 1, 0, 0);
+		
+		var bullet = game_state.bullets[id];
+		if (bullet) {
+			context.fillStyle = 'yellow';
+			context.beginPath();
+			context.arc(bullet.x, bullet.y, 4, 0, 2*Math.PI);
+			context.fill();
+		}
+		
+		context.fillStyle = 'black';
+		context.font = '12px serif';
+		context.textAlign = 'center';
+		context.fillText(player.score, x, y + 4);
+		
+	}
+	socket.emit('movement', movement);
 });
 
 
